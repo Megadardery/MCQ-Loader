338f63c9054d2a3c5ace30088a3443e8630cacfb
Ahmed Dardery <megadardery@yahoo.com>
2020-12-11 13:28:36 +0200

    chore: vs update

--- MCQ Loader/Properties/AssemblyInfo.cs
+++ MCQ Loader/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Management MCQs")]
+[assembly: AssemblyTitle("MCQ Loader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("Management MCQs")]
+[assembly: AssemblyProduct("MCQ Loader")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
